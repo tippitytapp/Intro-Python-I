3f2f416cb16bcd592ae9a7b5b19836dce52a94b2
Marc Tapp <marc.tapp@gmail.com>
2020-06-02 11:11:47 -0400

    got the args working, now for cal

--- src/11_args.py
+++ src/11_args.py
@@ -15,7 +15,7 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 def f2(*args):
-    print(sum(list(args)))
+    return sum(args)
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -25,7 +25,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+# print(f2(tuple(a)))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -35,8 +35,12 @@ print(f2(a))    # Should print 22
 # YOUR CODE HERE
 def f3(*args):
     length = len(args)
+    if length == 1:
+        return sum(list(args), 1)
+    else:
+        return sum(args)
 
-    
+
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
@@ -50,6 +54,11 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    for key in kwargs:
+        print(f'key: {key}, value: {kwargs[key]}')
+    # for k, v in enumerate(kwargs):
+    #     print(f'key: {v}, value: {v.value()}')
 
 # Should print
 # key: a, value: 12
@@ -68,4 +77,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d)
